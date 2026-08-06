296cf75bb92e8ceffdf269deed9e33c8a137cf62
rk-exxec <40037381+rk-exxec@users.noreply.github.com>
2026-08-06 22:59:17 +0200

    reformat config file

--- src/main/java/com/rk_exxec/random_bag/CommonConfig.java
+++ src/main/java/com/rk_exxec/random_bag/CommonConfig.java
@@ -7,16 +7,16 @@ public class CommonConfig {
     public static final ForgeConfigSpec.Builder BUILDER = new ForgeConfigSpec.Builder();
     public static final ForgeConfigSpec SPEC;
 
-    public static final ForgeConfigSpec.ConfigValue<List<? extends String>> ALLOWED_RESOURCE_LOCATIONS;
+    public static final ForgeConfigSpec.ConfigValue<List<? extends String>> ALLOWED_STRUCTURE_POOLS;
     public static final ForgeConfigSpec.ConfigValue<List<? extends Integer>> BAG_SIZE_MULTS;
     public static final Integer defaultMult = 1;
 
     static {
-        BUILDER.push("Random Bag Structure Select Config");
+        BUILDER.push("random_bag");
 
-        ALLOWED_RESOURCE_LOCATIONS = BUILDER.comment("A list of structure resource locations with optional wildcard patterns the selection algorithm should apply to. E.g. 'compstruct:*_boulder'. 'minecraft:village*', 'minecraft:village/desert/*'")
-                .defineList("Allowed resource locations", List.of("compstruct:*_boulder"), loc -> loc instanceof String);
-        BAG_SIZE_MULTS = BUILDER.comment("A list of multipliers that specifies how often the bag should contain all templates of each structure type.")
+        ALLOWED_STRUCTURE_POOLS = BUILDER.comment("#\n A list of structure pool resource locations the selection algorithm should apply to (with optional wildcard patterns). \n E.g. 'minecraft:trail_ruins*', 'minecraft:village/desert/*', mydatapack:mystructurepool'.")
+                .defineList("Allowed structure pools", List.of("minecraft:village*"), loc -> loc instanceof String);
+        BAG_SIZE_MULTS = BUILDER.comment("#\n A list of multipliers that specifies how often the bag should contain all templates of each structure type. \n May allow for more variation.")
                 .defineList("Bag Size Multiplier", List.of(defaultMult), mult -> mult instanceof Integer);
 
         BUILDER.pop();
